d11c6c7f4adbef0a1b843d0ffb3f4f6862e31536
Mohan Datla <mdatla@microsoft.com>
2017-08-17 18:34:31 -0700

    Disabled the X64 building

--- setup.cmd
+++ setup.cmd
@@ -2,16 +2,17 @@
 echo .
 echo "Using CMAKE to set up Azure projects"
 echo .
+echo "Disabled x64 for now as the Azure SDK is throwing few errors-following up with Azure separately"
 cd deps\azure-iot-device-auth
 mkdir x86
-mkdir x64
+REM mkdir x64
 mkdir arm
 cd x86
 cmake -G "Visual Studio 14 2015" .. -Ddps_auth_type=tpm
 cd ..\arm
 cmake -G "Visual Studio 14 2015 ARM" .. -Ddps_auth_type=tpm
-cd ..\x64
-cmake -G "Visual Studio 14 2015 Win64" .. -Ddps_auth_type=tpm
+REM cd ..\x64
+REM cmake -G "Visual Studio 14 2015 Win64" .. -Ddps_auth_type=tpm
 
 
 echo .
@@ -29,11 +30,11 @@ msbuild dps_client\dps_client.vcxproj /p:TargetPlatformVersion=10.0.14393.0
 msbuild dps_client\dps_http_transport.vcxproj /p:TargetPlatformVersion=10.0.14393.0
 msbuild dps_client\tools\tpm_device_provision\tpm_device_provision.vcxproj /p:TargetPlatformVersion=10.0.14393.0
 
-cd ..\x64
-msbuild c-utility\aziotsharedutil.vcxproj /p:TargetPlatformVersion=10.0.14393.0
-msbuild dps_client\dps_client.vcxproj /p:TargetPlatformVersion=10.0.14393.0
-msbuild dps_client\dps_http_transport.vcxproj /p:TargetPlatformVersion=10.0.14393.0
-msbuild dps_client\tools\tpm_device_provision\tpm_device_provision.vcxproj /p:TargetPlatformVersion=10.0.14393.0
+REM cd ..\x64
+REM msbuild c-utility\aziotsharedutil.vcxproj /p:TargetPlatformVersion=10.0.14393.0
+REM msbuild dps_client\dps_client.vcxproj /p:TargetPlatformVersion=10.0.14393.0
+REM msbuild dps_client\dps_http_transport.vcxproj /p:TargetPlatformVersion=10.0.14393.0
+REM msbuild dps_client\tools\tpm_device_provision\tpm_device_provision.vcxproj /p:TargetPlatformVersion=10.0.14393.0
 
 
 echo .
@@ -42,5 +43,5 @@ echo .
 cd ..\..\..
 msbuild IotCoreDpsClient\IotCoreDpsClient.vcxproj /p:Platform=Win32
 msbuild IotCoreDpsClient\IotCoreDpsClient.vcxproj /p:Platform=ARM
-msbuild IotCoreDpsClient\IotCoreDpsClient.vcxproj /p:Platform=X64
+REM msbuild IotCoreDpsClient\IotCoreDpsClient.vcxproj /p:Platform=X64
 
